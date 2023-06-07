cf13d38c90ab8ff9d841ff722769b5ade5243f81
fujiwara <fujiwara.shunichiro@gmail.com>
2023-06-07 16:16:25 +0900

    bump version

--- maprobe.go
+++ maprobe.go
@@ -13,7 +13,7 @@ import (
 )
 
 var (
-	Version                = "0.5.1"
+	Version                = "0.5.4"
 	MaxConcurrency         = 100
 	MaxClientConcurrency   = 5
 	PostMetricBufferLength = 100
